d57304167321ece0e0d43784ecf3f1118d744e4f
worldmaxter <jdh1096@gmail.com>
2020-08-16 16:35:07 -0500

    Inicio xp sistem

---
+++ The Third Fiction/Assets/Scripts/Xp_Bar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Xp_Bar : MonoBehaviour
+{
+
+    public Slider slider;
+
+    public void setXp(int xp)
+    {
+        slider.value = xp;
+    }
+
+
+}
